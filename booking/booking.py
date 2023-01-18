2c59d0d68da09f98ab2e73cdd1b42cddb0eab9c8
castrombithisamm <castrombithi@live.com>
2023-01-19 00:06:16 +0300

    get currency

--- booking/booking.py
+++ booking/booking.py
@@ -4,12 +4,25 @@ from selenium import webdriver
 
 
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r"/home/castro/Desktop/"):
+    def __init__(self, driver_path=r"/home/castro/Desktop/", teardown=False):
         self.driver_path = driver_path
+        self.teardown = teardown
         os.environ['PATH'] += self.driver_path
-        super(Booking, self).__init__()
+        super(Booking, self).__init__() 
+        self.implicitly_wait(15)
+        self.maximize_window()
 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.teardown:
+            self.quit()
 
     def land_first_page(self):
         self.get(const.BASE_URL)
 
+
+    def change_currency(self, currency=None):
+        currency_element = self.find_element(
+                'css selector',
+                'button[data-testid="header-currency-picker-trigger"]'
+        )
+        currency_element.click()
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from booking.booking import Booking
 
-inst = Booking()
+# inst = Booking()
 
-inst.land_first_page()
+# inst.land_first_page()
+
+with Booking() as bot:
+    bot.land_first_page()
+    bot.change_currency()
